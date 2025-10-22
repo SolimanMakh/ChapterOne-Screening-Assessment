66d1be1fe7fa139d90b0c4864ac47c216b8dd280
SolimanTJB <solimanmakh@gmail.com>
2025-10-22 04:32:06 +0300

    clean

--- src/TaskManager/Screens/TaskAddEdit/TaskAddEdit.tsx
+++ src/TaskManager/Screens/TaskAddEdit/TaskAddEdit.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, TextInput, Button, Alert } from "react-native";
+import { View, Text, TextInput, Button, Alert, Platform } from "react-native";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { useTasks } from "../../TasksContext";
 import { styles } from "../../../styles";
@@ -9,47 +9,52 @@ type Props = NativeStackScreenProps<RootStackParamList, "AddEdit">;
 
 export default function AddEditTaskScreen({ route, navigation }: Props) {
 
-  // Grabbing the id from the route params to find out if it exist, if not then we are creating a new task
+    // Grabbing the id from the route params to find out if it exist, if not then we are creating a new task
 
-  const { id } = route.params || {};
-  const { tasks, add, update } = useTasks();
+    const { id } = route.params || {};
+    const { tasks, add, update } = useTasks();
 
-  const existing = id ? tasks.find((t) => t.id === id) : undefined;
-  const [text, setText] = React.useState(existing?.text ?? "");
-  const [description, setDescription] = React.useState(existing?.description ?? "");
+    const existing = id ? tasks.find((t) => t.id === id) : undefined;
+    const [text, setText] = React.useState(existing?.text ?? "");
+    const [description, setDescription] = React.useState(existing?.description ?? "");
 
 
-  const onSave = () => {
-    const textValue = text.trim();
-    const descriptionValue = description.trim();
+    const onSave = () => {
+        const textValue = text.trim();
+        const descriptionValue = description.trim();
 
-    if (!textValue || !descriptionValue ) {
-      Alert.alert("Please enter a task description");
-      return;
-    }
-    if (id && existing) update(id, textValue, descriptionValue);
-    else add(textValue, descriptionValue);
+        if (!textValue || !descriptionValue) {
 
-    navigation.goBack();
-  };
+            if (Platform.OS === "web") {
+                window.alert("Please fill in the task and task description properly")
+                return
+            }
+            Alert.alert("Please fill in the task and task description properly");
+            return;
+        }
+        if (id && existing) update(id, textValue, descriptionValue);
+        else add(textValue, descriptionValue);
 
-  return (
-    <View style={styles.container}>
-      <Text style={styles.label}>{id ? "Edit Task" : "Add Task"}</Text>
-      <TextInput
-        placeholder="Task"
-        value={text}
-        onChangeText={setText}
-        style={styles.input}
-      />
-        <TextInput
-        placeholder="Task description"
-        value={description}
-        onChangeText={setDescription}
-        style={styles.input}
-        multiline
-      />
-      <Button title="Save" onPress={onSave} />
-    </View>
-  );
+        navigation.goBack();
+    };
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.label}>{id ? "Edit Task" : "Add Task"}</Text>
+            <TextInput
+                placeholder="Task"
+                value={text}
+                onChangeText={setText}
+                style={styles.input}
+            />
+            <TextInput
+                placeholder="Task description"
+                value={description}
+                onChangeText={setDescription}
+                style={styles.input}
+                multiline
+            />
+            <Button title="Save" onPress={onSave} />
+        </View>
+    );
 }
